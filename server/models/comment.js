199760033a0078e3960844370d92d5cfd514dd2c
Tony-Coniglio <113558159+Tony-Coniglio@users.noreply.github.com>
2022-12-07 13:31:55 -0600

    comment and task models

---
+++ server/models/comment.js
@@ -0,0 +1,42 @@
+const { Schema, model, Types } = require('mongoose');
+
+// Author, User Recipient, Project Recipient, commentText 
+const commentSchema = new Schema(
+    {
+        username:
+        {
+            type: String,
+            required: true,
+        },
+
+        commentText:
+        {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 500
+        },
+
+        userRecipient:
+            [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'User'
+
+                }
+            ],
+
+        projectRecipient:
+            [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Project'
+                }
+            ]
+
+    }
+)
+
+
+const Comment = model('Comment', commentSchema);
+module.exports = Thought;
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user');
 const Project = require('./project');
-const Comment = require('./comment')
+const Comment = require('./comment');
+const Task = require('./task');
 
-module.exports = { User, Project, Comment };
+module.exports = { User, Project, Comment, Task };

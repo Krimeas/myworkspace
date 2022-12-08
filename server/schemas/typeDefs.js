04d49bbb68ba1d1b0560e47f5644f4c7cbd32977
PaytonRay <paytonray0810@gmail.com>
2022-12-08 12:48:53 -0600

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,43 @@
 // add createdAt and updatedAt fields for models that have {timestamps: true}
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        username: String
+        email: String
+        password: String
+        firstname: String
+        lastname: String
+        aboutme: String
+        profilepicture: String
+        friends: [user]!
+        projects: [project]!
+        comments: [comment]!
+        
+    }
+
+    type task {
+        taskName: String
+        project: [project]!
+    }
+    
+    type project {
+        projectName: String
+        projectDescription: String
+        owner: [user]!
+        members :[user]!
+        tasks: [task]!
+        comments: [comment]!
+        dueDate: Date
+    }
+
+    type comment {
+        username: String
+        commentText: String
+        userRecipient: [user]!
+        projectRecipient: [project]!
+    }
+
+    type Query {
+        
+    }

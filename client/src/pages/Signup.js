e16701b2b2c3453cdfc189924b443d9d7c4fa040
Krimeas <krimeass@gmail.com>
2022-12-12 01:05:25 -0600

    Changes to Signup form adding name variables and changing username to user.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-
 import { CREATE_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    username: '',
+    user: '',
+    firstName: '',
+    lastName: '',
     email: '',
     password: '',
   });
-  //   const [addUser, { error, data }] = useMutation(ADD_USER);
+    const [addUser, { error, data }] = useMutation(CREATE_USER);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -45,28 +46,47 @@ const Signup = () => {
             <div class="container">
               <h4 class="card-header bg-dark text-light p-2">Sign Up</h4>
             </div>
-
-            {/* {data ? 
-            (
-              <p>
-                <Link to="/">Go Home {' '}.</Link>
-              </p>
-            ) : ( */}
-
             <form onSubmit={handleFormSubmit}>
               <div class="container d-flex justify-content-center">
                 <div class="col-lg-4 col-lg-offset-4">
                   <div class="d-flex justify-content-center lign-items-center">
                     <div class="vstack gap-3">
 
+                    <div class="mb-3 mt-3">
+                        <div class="col-md-3 mb-5">
+                          <input
+                            className="form-input"
+                            placeholder="Create a username"
+                            name="user"
+                            type="text"
+                            value={formState.user}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+
                       <div class="mb-3 mt-3">
                         <div class="col-md-3 mb-5">
                           <input
                             className="form-input"
-                            placeholder="Your username"
-                            name="username"
+                            placeholder="Your First Name"
+                            name="firstName"
                             type="text"
-                            value={formState.name}
+                            value={formState.firstName}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+
+
+                      <div class="mb-3 mt-3">
+                        <div class="col-md-3 mb-5">
+                          <input
+                            className="form-input"
+                            placeholder="Your Last Name"
+                            name="lastName"
+                            type="text"
+                            value={formState.lastName}
                             onChange={handleChange}
                           />
                         </div>
@@ -89,7 +109,7 @@ const Signup = () => {
                         <div class="col-md-3 mb-5">
                           <input
                             className="form-input"
-                            placeholder="******"
+                            placeholder="Create a password"
                             name="password"
                             type="password"
                             value={formState.password}
@@ -101,13 +121,7 @@ const Signup = () => {
                   </div>
                 </div>
               </div>
-
             </form>
-
-
-
-
-
             <button
               className="btn btn-block btn-primary"
               style={{ cursor: 'pointer' }}
@@ -115,14 +129,7 @@ const Signup = () => {
             >
               Submit
             </button>
-
-
-
-
-
-            {/* )} */}
-
-            {/* {error && (
+            {error && (
               <div className="my-3 p-3 bg-danger text-white">
                 {error.message}
               </div>

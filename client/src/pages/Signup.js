d3d9afa402087f36b060cfdcff5e759af992bfc0
Krimeas <krimeass@gmail.com>
2022-12-12 01:15:54 -0600

    Alterations to styling.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { CREATE_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_USER } from "../utils/mutations";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    user: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
+    user: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
   });
-    const [addUser, { error, data }] = useMutation(CREATE_USER);
+  const [addUser, { error, data }] = useMutation(CREATE_USER);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -40,82 +40,70 @@ const Signup = () => {
 
   return (
     <main className="flex-row justify-center mb-4 row">
-      <div class="container h-100">
-        <div class="row h-100 justify-content-center align-items-center">
-          <div class="col-lg-4 col-lg-offset-4">
-            <div class="container">
-              <h4 class="card-header bg-dark text-light p-2">Sign Up</h4>
+      <div className="container h-100">
+        <div className="row h-100 justify-content-center align-items-center">
+          <div className="col-lg-12 col-lg-offset-4">
+            <div className="container col-lg-12">
+              <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
             </div>
             <form onSubmit={handleFormSubmit}>
-              <div class="container d-flex justify-content-center">
-                <div class="col-lg-4 col-lg-offset-4">
-                  <div class="d-flex justify-content-center lign-items-center">
-                    <div class="vstack gap-3">
-
-                    <div class="mb-3 mt-3">
-                        <div class="col-md-3 mb-5">
-                          <input
-                            className="form-input"
-                            placeholder="Create a username"
-                            name="user"
-                            type="text"
-                            value={formState.user}
-                            onChange={handleChange}
-                          />
-                        </div>
+              <div className="container d-flex justify-content-center">
+                <div className="col-lg-12 col-lg-offset-4">
+                  <div className="d-flex justify-content-center lign-items-center">
+                    <div className="vstack gap-3 col-lg-10">
+                      <div className="mb-3 mt-3">
+                        <input
+                          className="form-control"
+                          placeholder="Create a username"
+                          name="user"
+                          type="text"
+                          value={formState.user}
+                          onChange={handleChange}
+                        />
                       </div>
 
-                      <div class="mb-3 mt-3">
-                        <div class="col-md-3 mb-5">
-                          <input
-                            className="form-input"
-                            placeholder="Your First Name"
-                            name="firstName"
-                            type="text"
-                            value={formState.firstName}
-                            onChange={handleChange}
-                          />
-                        </div>
+                      <div className="mb-3 mt-3">
+                        <input
+                          className="form-control"
+                          placeholder="Your First Name"
+                          name="firstName"
+                          type="text"
+                          value={formState.firstName}
+                          onChange={handleChange}
+                        />
                       </div>
 
-
-                      <div class="mb-3 mt-3">
-                        <div class="col-md-3 mb-5">
-                          <input
-                            className="form-input"
-                            placeholder="Your Last Name"
-                            name="lastName"
-                            type="text"
-                            value={formState.lastName}
-                            onChange={handleChange}
-                          />
-                        </div>
+                      <div className="mb-3 mt-3">
+                        <input
+                          className="form-control"
+                          placeholder="Your Last Name"
+                          name="lastName"
+                          type="text"
+                          value={formState.lastName}
+                          onChange={handleChange}
+                        />
                       </div>
 
-                      <div class="mb-3 mt-3">
-                        <div class="col-md-3 mb-5">
-                          <input
-                            className="form-input"
-                            placeholder="Your email"
-                            name="email"
-                            type="email"
-                            value={formState.email}
-                            onChange={handleChange}
-                          />
-                        </div>
+                      <div className="mb-3 mt-3">
+                        <input
+                          className="form-control"
+                          placeholder="Your email"
+                          name="email"
+                          type="email"
+                          value={formState.email}
+                          onChange={handleChange}
+                        />
                       </div>
 
-                      <div class="mb-3 mt-3">
-                        <div class="col-md-3 mb-5">
-                          <input
-                            className="form-input"
-                            placeholder="Create a password"
-                            name="password"
-                            type="password"
-                            value={formState.password}
-                            onChange={handleChange}
-                          />
-                        </div>
+                      <div className="mb-3 mt-3">
+                        <input
+                          className="form-control"
+                          placeholder="Create a password"
+                          name="password"
+                          type="password"
+                          value={formState.password}
+                          onChange={handleChange}
+                        />
                       </div>
                     </div>
                   </div>
@@ -124,7 +112,7 @@ const Signup = () => {
             </form>
             <button
               className="btn btn-block btn-primary"
-              style={{ cursor: 'pointer' }}
+              style={{ cursor: "pointer" }}
               type="submit"
             >
               Submit
@@ -137,7 +125,7 @@ const Signup = () => {
           </div>
         </div>
       </div>
-    </main >
+    </main>
   );
 };
 
